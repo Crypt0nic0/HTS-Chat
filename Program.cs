f39c3a2f2ed8ac6b14e9b896ac93af94952ced4f
Crypt0nic0 <n.greneche45@gmail.com>
2023-04-13 12:38:32 +0200

    socket / endpoint tutorial OK

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+
+try
+{
+    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+    IPAddress add = IPAddress.Any;
+    IPEndPoint endpoint = new IPEndPoint(add, 2345);
+
+    socket.Bind(endpoint);
+    socket.Listen();
+    socket.Accept();
+    Console.WriteLine("Connexion OK");
+}
+catch
+{
+    Console.WriteLine("Erreur lors de la connexion");
+}
