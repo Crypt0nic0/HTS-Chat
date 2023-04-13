21458db0316303a0633d151b6164addf660a80e6
Crypt0nic0 <n.greneche45@gmail.com>
2023-04-13 13:28:31 +0200

    first exchange with server

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Net;
 using System.Net.Sockets;
-
+using System.Text;
 
 try
 {
@@ -11,8 +11,15 @@ try
 
     socket.Bind(endpoint);
     socket.Listen();
-    socket.Accept();
-    Console.WriteLine("Connexion OK");
+    var socketcli = socket.Accept();
+    Console.WriteLine("Connexion : OK");
+    if (socketcli.RemoteEndPoint is not null)
+    {
+        Console.WriteLine($"Client connecté depuis l'adresse : {socketcli.RemoteEndPoint.ToString()}");
+        byte[] buffer = new byte[128];
+        int nb = socketcli.Receive(buffer);
+        Console.WriteLine($"Message reçu : {Encoding.ASCII.GetString(buffer, 0, nb)}");
+    }
 }
 catch
 {
